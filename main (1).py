ee98d3c17bc80c0027020067cf3592e1a9ddfcda
AdrianRenna <80462852+AdrianRenna@users.noreply.github.com>
2021-04-03 19:55:04 -0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+row_1 = ['Facebook', 0.0, 'USD', 12411515, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 234234, 4.5]
+row_3 = ['Youtube', 0.0, 'USD', 54646323, 4.5]
+row_4 = ['Google', 0.0, 'USD', 78678, 5.0]
+row_5 = ['Viber', 0.0, 'USD', 4565645, 3]
+row_6 = ['Tiktok', 0.0, 'USD', 789789789, 3.5]
+
+data_set = [row_1, row_2, row_3, row_4, row_5, row_6]
+print(data_set[0])
+print(data_set[1])
+print(data_set[2])
+print(data_set[3])
+print(data_set[4])
+print(data_set[5])
